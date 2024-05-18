82f9d1adf27735b5e2d538b20b34f836942ac7ab
Dabidski21 <qcdsuzuki01@tip.edu.ph>
2024-05-18 12:51:18 +0800

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = tf.keras.models.load_model('cifar10_classifier.hdf5')
+    model = tf.keras.models.load_model('CNN.hdf5')
     return model
 
 model = load_model()
